a57129e1a4d8c6c39a41302c79490bb987dd13bd
Jen Doyle <jdoyle5254@users.noreply.github.com>
2021-02-24 17:14:55 -0600

    added styling and updated search functionality

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,38 +1,18 @@
 
 import Header from "./components/Header.js"
-import SearchBox from "./components/SearchBox"
-import Table from "./components/Table"
+// import SearchBox from "./components/SearchBox"
+// import Table from "../workinprogress/Table"
 import Footer from "./components/Footer"
-import React, {useState, useEffect} from "react"
+import Employee from "./components/Employee"
+import React from "react"
 import './App.css';
 
 function App() {
-  const [users, setUsers] = useState([])
-  
-  useEffect(() => {
-    console.log('useEffect')
-  
-    const fetchData = async () => {
-        try {
-          const response = await fetch ('https://randomuser.me/api/?results=10')
-          const data = await response.json()
-          setUsers([...data.results])
-        } catch(e) {
-          console.log(e); 
 
-        }
-    }
-    fetchData()
-    
-  }, [])
-
-  console.log('users1', users)
-  useEffect(() => (console.log('users2', users)))
   return (
     <div className="App">
     <Header />
-    <SearchBox />
-    <Table />
+    <Employee />
     <Footer />
      </div>
   );
